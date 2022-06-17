741331e0e345c6b27042a62a513f7cf1ca272f73
KensenH <kensen.huang@gmail.com>
2022-06-17 22:31:30 +0700

    add a little bit detail

--- webhook-server/setup.sh
+++ webhook-server/setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 #script to automate admission controller and webhook server deployment
+
+
+# please, make sure change kube context to desired cluster
+
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+#download dependencies
+# source ./dependencies.sh
+
+#generate tls certificate
+source ./gen-certs.sh
+
+#build docker image and push
+# DOCKER_BUILDKIT=1 docker build . -t kensenh/simple-kubernetes-webhook
+# docker push kensenh/simple-kubernetes-webhook:latest
+
+#deploy
