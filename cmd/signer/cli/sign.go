c85bfed1ad006d0e2f23a3192c5801b8a92c3982
KensenH <kensen.huang@gmail.com>
2022-06-20 22:50:53 +0700

    signer

---
+++ cmd/signer/cli/sign.go
@@ -0,0 +1,109 @@
+package cli
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/sigstore/k8s-manifest-sigstore/pkg/k8smanifest"
+	k8smnfutil "github.com/sigstore/k8s-manifest-sigstore/pkg/util"
+	kubeutil "github.com/sigstore/k8s-manifest-sigstore/pkg/util/kubeutil"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+const filenameIfInputIsDir = "manifest.yaml"
+
+func NewCmdSign() *cobra.Command {
+
+	var imageRef string
+	var inputDir string
+	var keyPath string
+	var output string
+	var applySignatureConfigMap bool
+	var updateAnnotation bool
+	var imageAnnotations []string
+	cmd := &cobra.Command{
+		Use:   "sign -f FILENAME [-i IMAGE]",
+		Short: "A command to sign Kubernetes YAML manifests",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			err := sign(inputDir, imageRef, keyPath, output, applySignatureConfigMap, updateAnnotation, imageAnnotations)
+			if err != nil {
+				log.Fatalf("error occurred during signing: %s", err.Error())
+				return nil
+			}
+			return nil
+		},
+	}
+
+	cmd.PersistentFlags().StringVarP(&inputDir, "filename", "f", "", "file name which will be signed (if dir, all YAMLs inside it will be signed)")
+	cmd.PersistentFlags().StringVarP(&imageRef, "image", "i", "", "image name which bundles yaml files and be signed")
+	cmd.PersistentFlags().StringVarP(&output, "output", "o", "", "output file name or k8s signature configmap reference (if empty, use `<filename>.signed`)")
+	cmd.PersistentFlags().StringVarP(&keyPath, "key", "k", "", "path to your signing key (if empty, do key-less signing)")
+	cmd.PersistentFlags().BoolVar(&applySignatureConfigMap, "apply-signature-configmap", false, "whether to apply a generated signature configmap only when `output` is k8s configmap")
+	cmd.PersistentFlags().BoolVar(&updateAnnotation, "annotation-metadata", true, "whether to update annotation and generate signed yaml file")
+	cmd.PersistentFlags().StringArrayVarP(&imageAnnotations, "annotation", "a", []string{}, "extra key=value pairs to sign")
+
+	return cmd
+}
+
+func sign(inputDir, imageRef, keyPath, output string, applySignatureConfigMap, updateAnnotation bool, annotations []string) error {
+	if output == "" && updateAnnotation {
+		if isDir, _ := k8smnfutil.IsDir(inputDir); isDir {
+			// e.g.) "./yamls/" --> "./yamls/manifest.yaml.signed"
+			output = filepath.Join(inputDir, filenameIfInputIsDir+".signed")
+		} else {
+			// e.g.) "configmap.yaml" --> "configmap.yaml.signed"
+			output = inputDir + ".signed"
+		}
+	}
+
+	var anntns map[string]interface{}
+	var err error
+	if len(annotations) > 0 {
+		anntns, err = parseAnnotations(annotations)
+		if err != nil {
+			return err
+		}
+	}
+
+	so := &k8smanifest.SignOption{
+		ImageRef:         imageRef,
+		KeyPath:          keyPath,
+		Output:           output,
+		UpdateAnnotation: updateAnnotation,
+		ImageAnnotations: anntns,
+	}
+
+	if applySignatureConfigMap && strings.HasPrefix(output, kubeutil.InClusterObjectPrefix) {
+		so.ApplySigConfigMap = true
+	}
+
+	_, err = k8smanifest.Sign(inputDir, so)
+	if err != nil {
+		return err
+	}
+	if so.UpdateAnnotation {
+		finalOutput := output
+		if strings.HasPrefix(output, kubeutil.InClusterObjectPrefix) && !applySignatureConfigMap {
+			finalOutput = k8smanifest.K8sResourceRef2FileName(output)
+		}
+		log.Info("signed manifest generated at ", finalOutput)
+	}
+	return nil
+}
+
+func parseAnnotations(annotations []string) (map[string]interface{}, error) {
+	annotationsMap := map[string]interface{}{}
+
+	for _, annotation := range annotations {
+		kvp := strings.SplitN(annotation, "=", 2)
+		if len(kvp) != 2 {
+			return nil, fmt.Errorf("invalid flag: %s, expected key=value", annotation)
+		}
+
+		annotationsMap[kvp[0]] = kvp[1]
+	}
+	return annotationsMap, nil
+}
